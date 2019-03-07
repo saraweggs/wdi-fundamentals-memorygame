8df46a1dc59041671f6b863012be6d58154b9e9a
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-03-07 16:57:19 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,41 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardId = cards[0, 1, 2, 3];
 
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
 if (cardsInPlay.length === 2) {
 if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else {
-	alert("Sorry, try again.")
+	alert("Sorry, try again.");
+}
+checkForMatch();
 }
 }
+
+cardsInPlay.push(cards[cardId]);
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+// logic to see if two cards have been played
+/*if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+} */
