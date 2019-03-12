a5da95db6712d94b2c13315cbe083eb3902824ca
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-03-12 19:40:12 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = []
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
 
 var checkForMatch = function() {
@@ -15,13 +32,16 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].suit);
+console.log("User flipped " + cards[cardId].cardImages);
+cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+		checkForMatch();
 	}
 }
-}
-checkForMatch();
+
 
 flipCard(0);
 flipCard(2);
+
