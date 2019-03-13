20944ff7c14ece1de65b5cbfeaf2aba1810a8b7b
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-03-12 22:39:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,17 +31,27 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId].rank);
-console.log("User flipped " + cards[cardId].suit);
-console.log("User flipped " + cards[cardId].cardImages);
-cardsInPlay.push(cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	//console.log("User flipped " + cards[cardId].rank);
+	//console.log("User flipped " + cards[cardId].suit);
+	//console.log("User flipped " + cards[cardId].cardImages);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
